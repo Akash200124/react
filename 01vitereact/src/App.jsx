ee09ea94e26392bef6d25a45a80e919270b571ea
unknown <akashkumar200124@gmail.com>
2024-08-02 08:35:13 +0530

    custom react created

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 
 import Fun from "./akash"
 function App() {
-  
+  const username = "akashkumar"
   return (
     <>
     <Fun/>
     <h1>we have to retun only one element so we can wrap in a div or `<> </>` it is called as fragment </h1>
     <p>fragment </p>
+    <h2>this {username} this is how we write the variable and final evaluated expression final outcome of the js </h2>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>custom App| akash</h1>
+        </div>
+    )
+}
+// const reactElement = {
+//     type: "a",
+//     props:{
+//        href:'https://google.com',
+//        target:"_blank"
+//     },
+//     children:"click me to visit google"   
+//    }
+
+   const anotherhrefElement = (
+    <a href="https://google.com" target='_blank'>Visite google  </a>
+   )
+
+   const anotheruser = "akashuser";
+   const reactElement = React.createElement(
+    'a',
+    {href:"https:mobiheal.in",target:"_blank"},
+    'click me to vist mobiheal',
+    anotheruser
+   )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    // MyApp()
+    // reactElement
+    // anotherhrefElement
+    reactElement
 )
